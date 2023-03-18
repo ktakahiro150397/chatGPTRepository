a34f3e41be9bfda6a3ed74e9c9fb64d6faeb204b
ktakahiro150397_rasbperrypi.local <k_takahiro_150397@yahoo.co.jp>
2023-03-18 17:08:42 +0900

    add sumArray function with template

--- cpp_addFunction.cpp
+++ cpp_addFunction.cpp
@@ -4,8 +4,9 @@ cpp
 
 using namespace std;
 
-int sumArray(int *arr, int size) {
-    int sum = 0;
+template <typename T>
+T sumArray(T *arr, int size) {
+    T sum = 0;
     for (int i = 0; i < size; i++) {
         sum += *(arr + i);
     }
@@ -22,11 +23,11 @@ int main() {
         cerr << msg << endl;
     }
 
-    int* nullArr = NULL; // a sample null array
-    size = 0; // size of null array is zero
+    double doubleArr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    size = sizeof(doubleArr) / sizeof(doubleArr[0]);
     try {
-        int sum = sumArray(nullArr, size);
-        cout << "Sum of null array: " << sum << endl;
+        double doubleSum = sumArray(doubleArr, size);
+        cout << "Sum of double array: " << doubleSum << endl;
     } catch (const char* msg) {
         cerr << msg << endl;
     }
